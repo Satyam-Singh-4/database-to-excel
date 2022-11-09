efc340c0a4f3c8eff10d5b17b8f768a95bfa9d95
Satyam-Singh-4 <satyam7619patel@gmail.com>
2022-11-09 15:00:30 +0530

    commit

--- router/router.js
+++ router/router.js
@@ -1,9 +1,9 @@
-const router=require('express').Router()
-//const studentController=require('../controller/excelController')
-const controller1=require('../controller/controller')
+const router = require("express").Router();
+const studentController = require("../controller/excelController");
+//const controller1 = require("../controller/controller");
 
-router.post('/create',studentController.createStudent)
+router.post("/create", studentController.createStudent);
 //router.get('/get',studentController.createExcel)
-router.get('/get1',controller1.exportStudent)
+router.get("/get1", studentController.exportStudent);
 
-module.exports=router
+module.exports = router;
